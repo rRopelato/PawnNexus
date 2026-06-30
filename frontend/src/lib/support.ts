84496df7fa9798ac3d17be5a546c98b768c5b223
rRopelato <r.ropelato@proton.me>
2026-06-29 21:34:45 -0300

    Initial PawnNexus portfolio project

--- frontend/src/lib/support.ts
+++ frontend/src/lib/support.ts
@@ -1,6 +1,6 @@
 export const creator = {
   name: 'rRopelato',
   githubUrl: 'https://github.com/rRopelato',
-  koFiUrl: 'https://ko-fi.com/rropelato',
+  koFiUrl: 'https://ko-fi.com/J0L521F1CI',
   pixQrCodeUrl: 'https://raw.githubusercontent.com/rRopelato/PawnNexus/refs/heads/main/PixIMG.jpeg',
 };
