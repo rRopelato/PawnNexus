f4da411e36fb1562b4473879aa05c3f301495a83
Renan Ropelato <155210228+rRopelato@users.noreply.github.com>
2026-06-29 22:16:52 -0300

    Update pixQrCodeUrl to new CDN link

--- frontend/src/lib/support.ts
+++ frontend/src/lib/support.ts
@@ -2,5 +2,5 @@ export const creator = {
   name: 'rRopelato',
   githubUrl: 'https://github.com/rRopelato',
   koFiUrl: 'https://ko-fi.com/J0L521F1CI',
-  pixQrCodeUrl: 'https://pub-c5f8bcd4db314efc8420e0c7bd213b24.r2.dev/PixIMG.webp',
+  pixQrCodeUrl: 'https://cdn.pawnnexus.com/PixIMG.webp',
 };
